2c5ded409f9a68743666806af8ecb3364c194fa7
minhtien100396 <minhtien100396@gmail.com>
2022-11-21 16:58:38 +0700

    module 5

---
+++ casestudy_module_5/case_study/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from "./home/header/header.component";
+import {NavbarComponent} from './home/navbar/navbar.component';
+import {ContentComponent} from './home/content/content.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { ListComponent } from './facility/list/list.component';
+import { EditComponent } from './facility/edit/edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent,
+    ListComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
